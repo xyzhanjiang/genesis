213fe461e0b83e54b4599c555b45f0b22da0bd6c
xyzhanjiang <xyzhanjiang@qq.com>
2017-06-16 18:03:53 +0800

    first commit

--- README.md
+++ README.md
@@ -9,14 +9,14 @@
 PS. 括号后面为样式对应文件
 
 - [x] normalize(normalize.css)
-- [ ] 排版(type.css)
+- [x] 排版(type.css)
 - [ ] 网格(grid.css)
-- [ ] 表单(form.css)
-- [ ] 按钮(button.css)
-- [ ] 顶部/底部菜单(bar.css)
-- [ ] 列表(list.css)
-- [ ] 图标(icon.css)
-- [ ] 工具类(utility.css)
+- [x] 表单(form.css)
+- [x] 按钮(button.css)
+- [x] 顶部/底部菜单(bar.css)
+- [x] 列表(list.css)
+- [x] 图标(icon.css)
+- [x] 工具类(utility.css)
 
 ## 组件
 
---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+const cssnext = require('postcss-cssnext');
+const cssnano = require('cssnano');
+const concat = require('gulp-concat');
+const sourcemaps = require('gulp-sourcemaps');
+const rename = require('gulp-rename');
+const browserSync = require('browser-sync').create();
+const reload = browserSync.reload;
+
+const scripts = {
+  css: [
+    './src/css/variables.css',
+    './node_modules/normalize.css/normalize.css',
+    './src/css/base.css',
+    './src/css/type.css',
+    './src/css/forms.css',
+    './src/css/badges.css',
+    './src/css/bars.css',
+    './src/css/buttons.css',
+    './src/css/lists.css',
+    './src/css/icons.css',
+    './src/css/utilities.css',
+  ]
+};
+
+// css
+gulp.task('css', () =>
+  gulp.src(scripts.css)
+    .pipe(sourcemaps.init())
+    .pipe(concat('genesis.css'))
+    .pipe(postcss([
+      cssnext()
+    ]))
+    .pipe(gulp.dest('./dist/css'))
+    .pipe(postcss([cssnano({autoprefixer: false})]))
+    .pipe(rename({extname: '.min.css'}))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('dist/css'))
+);
+
+// 启动服务
+gulp.task('serve', () => {
+  browserSync.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+
+  gulp.watch('src/css/**/*.css', ['css']);
+  gulp.watch('dist/*.html').on('change', reload);
+});
+
+gulp.task('default', ['css']);
